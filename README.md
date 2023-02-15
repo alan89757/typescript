2e88a90eb6fa31484bc88c0de06d75c00ac12499
Alan <liangrongjun@cosla.com>
2023-02-15 18:06:00 +0800

    泛型

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 npm i -g typescript   // 全局安装typescript
 npm i -g ts-node  // 运行ts文件, 类似1. tsc index.ts 2. node index.js
 ```
+
+## 未熟悉的知识点
+```js
+infer
+ReturnType
+InstanceType
+```
--- index.ts
+++ index.ts
@@ -1,194 +1,171 @@
-// 八种数据类型: undefined, null, boolean, number, string, object, symbol, bigint
-// 非严格模式下, undefined和null是所有类型的子类型
-// let str: string = 'a';
-// str = 'b';
-// console.log('111--', str);
+// 11. Parameters 用于获得函数的参数类型所组成的元组类型。
+// type P1 = Parameters<(a: number, b: string)=>void>; // [number, string]
 
-// 枚举
-// enum Color {
-//   RED,
-//   PINK,
-//   BLUE,
+// // 12. InstanceType 返回构造函数类型T的实例类
+// class C {
+//   x = 0;
+//   y = 0;
+// }
+// type D = InstanceType<typeof C>;  // C
+
+// 泛型
+// interface getLength {
+//   length: number
 // }
 
-// 元组类型(tuple)
-// const tuple: [number, string] = [1, 'a']
+// function getValue<T extends getLength>(val: T): T {
+//   console.log(val.length)
+//   return val;
+// }
+// getValue('1')
 
-// void类型
-// function hello(): void {
-//   console.log(111);
+// typeof
+// let p1 = {
+//   name: 'alan',
+//   age: 20,
+//   gender: 'male'
 // }
 
-// never类型
-// function error(): never {
-//   throw new Error('error')
+// type People = typeof p1;
+
+// function getName(p: People): string {
+//   return p.name
 // }
+// getName({})
 
-// function error1(): never {
-//   while(true) {}
-// }
-
-// unknown类型
-// 对比unknown和any类型
-// let value1: any = 1;
-// let value2: unknown = 1;
-
-// let value3: number = value1;  // any可以赋值给任何类型
-// let value4: number = value2;  // 不能将类型“unknown”分配给类型“number”, unknown只能赋值给unknown和any
-// let value5: any = value2;  // unknown可以赋值给any
-// let value6: unknown = value2;  // unknown可以赋值给unknown
-
-// let obj1: object = undefined;  // 不能将类型“undefined”分配给类型“object”
-// let obj2: object = null; // 不能将类型“null”分配给类型“object”
-// let obj3: object = 1; // 不能将类型“number”分配给类型“object”
-// let obj4: object = 'a'; // 不能将类型“string”分配给类型“object”
-// let obj5: object = true;  // 不能将类型“boolean”分配给类型“object”
-// let obj6: object = {} // 正确
-
-// let obj1: Object = undefined; // 不能将类型“undefined”分配给类型“Object”
-// let obj2: Object = null; // 不能将类型“null”分配给类型“Object”
-// let obj3: Object = 1; // OK
-// let obj4: Object = 'a'; // OK
-// let obj5: Object = true; // OK
-// let obj6: Object = {} // OK
-
-// let obj1: {} = undefined; // 不能将类型“undefined”分配给类型“{}”
-// let obj2: {} = null; // 不能将类型“null”分配给类型“{}”
-// let obj3: {} = 1; // OK
-// let obj4: {} = 'a'; // OK
-// let obj5: {} = true; // OK
-// let obj6: {} = {} // OK
-
-// 声明类
-// class Person {
-//   name: string;
-//   age: number;
-//   constructor(name: string, age: number) {
-//     this.name = name;
-//     this.age = age;
-//   }
-//   sayHi(): void {
-//     console.log(`hello ${this.name}`)
-//     // return `hello ${this.name}`
-//   }
-// }
-// const person = new Person('alan', 18);
-// person.sayHi()
-
-// 函数声明
-// function add(num1: number, num2: number): number {
-//   return num1 + num2;
-// }
-
-// const add1 = function(num1: number, num2: number): number {
-//   return num1 + num2;
-// }
-
-// 接口定义
+// keyof
 // interface Person {
 //   name: string,
+//   age: number,
+//   gender: 'male' | 'female'
+// }
+
+// type PersonKey = keyof Person
+
+// function getValueByKey(p: Person, key: PersonKey) {
+//   return p[key]
+// }
+
+// getValueByKey({ name: "alan", age: 18, gender: "male" }, 'age')
+
+// in
+// type Keys = 'a' | 'b' | 'c';
+
+// type Obj = {
+//   [p in Keys]: number
+// }
+
+// let obj1: Obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3
+// }
+
+// infer 没理解透
+type ReturnType1<T> = T extends (...args: any[]) => infer R ? R : any;
+
+// 索引访问符
+// interface Person {
+//   name: string;
 //   age: number
 // }
 
-// // let p1: Person = null;   // 不能将类型“null”分配给类型“Person”
-// let p2: Person = {  // OK
-//   name: 'alan',
-//   age: 18,
-// }
-
-// interface fn {
-//   (msg: string): number;
-// }
-
-// // 定义函数只能用与函数表达式
-// let fn1: fn = function(msg: string): number {
-//   return +msg;
-// }
-
-// interface Iadd {
-//   (num1: number, num2: number, extra?: number): void
-// }
-
-// let add: Iadd = function() {
-  
-// }
+// type x = Person['name'];
+// let x1:x = 'a';
 
 
-// 函数声明
-// function fn1(num1: number, num2: number): number {
-//   return num1 + num2;
+// Required
+// interface Person {
+//   name?: string,
+//   age?: number,
+//   hobby?: string[]
 // }
 
-// // 函数表达式
-// let add = function(num1: number, num2: number): number {
-//   return num1 + num2;
+// const user: Required<Person> = {
+//   name: 'alan'
 // }
 
-// 接口定义函数
-// interface IAdd  {
-//   (num1: number, num2?: number): number  // num2可选参数
+// Partial
+// interface Person {
+//   name: string,
+//   age: number,
+//   hobby: string[]
 // }
-// // num2设置默认值
-// let add1: IAdd = function(num1: number, num2: number = 2): number {
-//   return num1 + num2
-// }
-// add1(1);
 
-// // 接口定义对象
-// interface IPerson {
+// const user: Partial<Person> = {
+//   name: 'alan'
+// }
+
+// Exclude
+// type T0 = Exclude<'a' | 'b' | 'c', 'a'>
+// type T1 = Exclude<string | number | (()=> void), Function> 
+
+// // Extract 取并集
+// type T2 = Extract<'a' | 'b' | 'c', 'a' | 'f' | 'c'>; // "a"
+// type T3 = Extract<string | number | (()=>void), Function>; // void
+
+// readonly
+// interface Person {
 //   name: string;
-//   age?: number;  // 可选参数
+//   gender?: "male" | "female";
 // }
 
-// let p1: IPerson = {
+// let p1: Readonly<Person> = {
 //   name: 'alan',
+//   gender: 'male'
+// }
+// p1.name = 'alan2'
+
+// Record
+// type Property = 'key1' | 'key2';
+// type Person = Record<Property, string>;
+
+// const p: Person = {
+//   key1: 1,
+//   key2: 'a',
 // }
 
-// 剩余参数
-// function add(...nums: number[]): number {
-//   let sum = 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     sum +=nums[i];
-//   }
-//   return sum
+
+// Pick
+// type Person = {
+//   name: string;
+//   age:number;
+//   gender:string
 // }
 
-// // (定义)函数重载
-// type Types =  number | string;
+// type p1 = Pick<Person, 'name' | 'age'>;
 
-// function add(x: number, y: number): number;
-// function add(x: string, y: string): string;
-// function add(x: string, y: number): string;
-// function add(x: number, y: string): string;
-// function add(x: Types, y: Types): Types;
-
-// // 实现
-// function add (x: number, y: number): number {
-//   if(typeof x === 'string' || typeof y === 'string') {
-//     return x.toString() + y.toString();
-//   } else {
-//     return x + y;
-//   }
+// const user: p1 = {
+//   name: 'alan',
+//   age: 20
 // }
 
-// add(1,2)
-// add('1', '2')
-
-// function add3(x, y) {
-//   return x + y
+// Omit
+// interface Person {
+//   name: string,
+//   age: number,
+//   gender: string
+// }
+// type p2 = Omit<Person, 'age' | 'gender'>
+// const user2: p2 = {
+//   name: 'alan'
 // }
 
-type types = number | string
+// NonNullable
+type P1 = NonNullable<string | number | undefined | null>
 
-function add(x: number, y: number): number;
-function add(x: string, y: string): string;
+// ReturnType
+type Func = (value: string) => string;
+const test: ReturnType<Func> = '1'
 
-function add(x: types, y: types) {
-  if(typeof x === 'string' || typeof y === 'string') {
-    return x.toString() + y.toString();
-  }
-  return x + y;
- }
- add(1, 2); // 匹配第一个函数声明
- let result = add('aaa', 'bbb'); // 匹配第二个函数声明
- result.split('')
+// Parameters
+type P2 = Parameters<(a: number, b: string)=> void>;
+let arr: P2 = [1, 'a']
+
+// InstanceType
+class C {
+  x = 0;
+  y = 0;
+}
+type D = InstanceType<typeof C>;
+// let d1: D = class E {x=2; y=2}
